e5e9fe8e9b9375a3c8638e4b19ebf09a6d8efc3a
jhon <estebanortizgomez96@gmail.com>
2022-06-28 14:58:05 -0500

    Primer commit

---
+++ src/components/TodoCounter/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { TodoContext } from "../../Context";
+import './TodoCounter.css';
+
+function TodoCounter() {
+  const { totalTodos, completedTodos } = React.useContext(TodoContext);
+  return (
+    <h2 className="TodoCounter">Has completado {completedTodos} de {totalTodos} tareas</h2>
+  );
+}
+export { TodoCounter };
